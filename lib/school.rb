462e52446a137f6ea55e74601f7e5d5d4a77edd6
Rachel Grier <rgrier104@gmail.com>
2018-09-16 18:10:37 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,10 +1,11 @@
 # code here!
 class School
 
-  roster = {}
-
   def initialize(school_name)
     @school_name = school_name
     @roster = {}
   end
+
+  def add_student(name, grade)
+    @roster[:grade] = []
 end
